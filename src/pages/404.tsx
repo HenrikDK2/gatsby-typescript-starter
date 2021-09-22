a213532ecc616824d97bedc9f3ffc31bd7ec3f7e
HenrikDK2 <Henrik265@outlook.dk>
2021-09-22 16:09:42 +0200

    404 updated

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,18 +1,39 @@
 import React from "react";
-import { styled } from "goober";
+import { styled, css } from "goober";
 import Seo from "../components/Seo";
+import { Link } from "gatsby";
 
 const Heading = styled("h2")`
   text-align: center;
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
+`;
+
+const linkStyle = css`
+  text-align: center;
+  padding: 0.2em 1.45em;
+  border: 0.15em solid #cccccc;
+  box-sizing: border-box;
+  text-decoration: none;
+  color: #000000;
+  background-color: #cccccc;
+  border-radius: 4px;
+  transition: transform 0.2s;
+  &:hover {
+    border: 0.15em solid #999999;
+    background-color: #999999;
+  }
 `;
 
 const NotFoundPage: React.FunctionComponent = () => (
   <Seo title="404">
-    <Heading>Page not found</Heading>
+    <Heading>
+      Page not found{" "}
+      <span role="img" aria-label="Sad smiley">
+        😢
+      </span>
+    </Heading>
+    <Link className={linkStyle} to="/">
+      Go back to Homepage
+    </Link>
   </Seo>
 );
 
