75ad2274848626a5c354aede4c5234652a33e6aa
HenrikDK2 <Henrik265@outlook.dk>
2021-09-22 15:51:02 +0200

    Changed react import statement

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import { styled } from "goober";
 import { StaticImage } from "gatsby-plugin-image";
 import Seo from "../components/Seo";
